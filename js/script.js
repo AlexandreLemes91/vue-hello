bae25291e9c92b2fcbbc6afbb4f728b621cb102b
AlexandreLemes91 <lemesalexandre91@gmail.com>
2021-04-20 16:01:00 +0200

    FINALLY

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: "#app",
+
+    data: {
+        title: "Finalmente Vue!!!!!",
+        img: "https://media.tenor.com/images/244397c92a1f364b43e79d29bfc8773a/tenor.gif",
+    },
+
+})
+
+console.log("oi");
